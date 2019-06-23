4a9765068f490735c5246c001529e1e29869115f
shrisowdhaman <shrisowdhaman@gmail.com>
2019-06-23 10:41:52 -0400

    initial push

---
+++ README.md
@@ -0,0 +1,28 @@
+
+# Unsubscribe All  #
+
+Angular - Unsubscribe all Subscribed observable on ngDestroy using decorator @UnsubscribeAll()
+
+The subscriber do occupy space in memory and should be released (unsubscribe) of their space once we are done using them. @Unsubscribeall() release all memory once navigate out from the component.
+ 
+## Installation
+
+$ npm install -g unsubscribe-all 
+
+## Usage
+
+@UnsubscribeAll() // Call on ngDestory unsubscribe all subscribe observable
+@Component(){}
+export class TestComponent implements OnInit OnDestroy {
+
+private subscribe: Subscription;
+   constructor(private testService: TestService) { }
+
+  ngOnInit() {
+    this.subscribe = this.testService.callService().subscribe( res => {
+      console.log(res);
+    })
+  }
+
+  ngOnDestory{}
+}
---
+++ src/unsubscribe-all.ts
@@ -0,0 +1,54 @@
+'use strict';
+
+const isFunction = fn => typeof fn === "function";
+
+const doUnsubscribe = subscription => {
+  subscription &&
+    isFunction(subscription.unsubscribe) &&
+    subscription.unsubscribe();
+};
+
+const doUnsubscribeIfArray = subscriptionsArray => {
+  Array.isArray(subscriptionsArray) &&
+    subscriptionsArray.forEach(doUnsubscribe);
+};
+
+export function Unsubscribeall({
+  blackList = [],
+  arrayName = "",
+  event = "ngOnDestroy"
+} = {}) {
+  return function(target: Function) {
+    const original = target.prototype[event];
+
+    if (!isFunction(original)) {
+      // Throws the Error when NgDestory no declared in the component
+      throw new Error(
+        `${
+            target.name
+        } is using @UnsubscribeAll but does not implement OnDestroy`
+      );
+
+      console.warn(`${target.name} is using @UnsubscribeAll but has not declared ${event}`);
+    }
+
+    target.prototype[event] = function() {
+      if (arrayName) {
+        doUnsubscribeIfArray(this[arrayName]);
+        isFunction(original) && original.apply(this, arguments);
+        return;
+      }
+
+      for (let propName in this) {
+        if (blackList.includes(propName)) continue;
+
+        const property = this[propName];
+        doUnsubscribe(property);
+      }
+
+      isFunction(original) && original.apply(this, arguments);
+    };
+  };
+
+  
+}
